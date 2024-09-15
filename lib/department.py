37d9d19a5b2bbf410831e5c6221521825f5e5cb1
Silvanos Eric <ericoderito@gmail.com>
2024-09-15 14:56:16 +0300

    feat(lib): add create_table and drop_table methods to Department class
    Added methods to the Department class to create and drop the database table for persisting
    Department instances.
    
    - create_table: Creates a new table if it does not exist
    - drop_table: Drops the existing table
    
    This change helps in managing the database schema for the Department entities.

--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,26 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        """ Create a new table to persist the attributes of Department instances """
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            );
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """ Drop the table that persits Department instances """
+        sql = """
+            DROP TABLE IF EXISTS departments;
+        """
+
+        CURSOR.execute(sql)
+        CONN.commit()
